1b8b1fbed1c76c2513fa39a64bf2da414c05f590
Magdalena <kokanovic422@gmail.com>
2022-10-16 15:00:20 +0200

    ...

--- cr3/src/app/cart/cart.component.ts
+++ cr3/src/app/cart/cart.component.ts
@@ -12,9 +12,16 @@ export class CartComponent implements OnInit {
 
   items: IDishes[] = [];
 
-  sum=0;
 
-  makeSumm():number {
+  sum = 0;
+
+  service = 0;
+
+  discount = 0;
+
+  total = 0;
+
+  calculateSum():number {
 
   for(let item of this.items) {
 
@@ -27,15 +34,40 @@ export class CartComponent implements OnInit {
 
   }
 
+  calculateService(): number {
+
+
+    this.service = this.sum / 100 * 10
+
+    return this.service
+  }
+
+  calculateDiscount(): number {
+
+    if (this.sum + this.service > 40) {
+      this.discount = (this.sum + this.service) / 100 * 15
+
+    }
+
+    return this.discount
+  }
+
+  calculateTotal(): number {
+
+
+    this.total=this.sum+this.service+this.discount
+
+    return this.total
+  }
 
   constructor(private cartService:CartService) { }
 
   ngOnInit(): void {
-   
-  
+
+
     this.items=this.cartService.getItems();
 
-     
+
 
 
   }
